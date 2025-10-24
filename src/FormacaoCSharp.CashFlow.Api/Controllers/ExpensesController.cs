a60ec70e06bf70e50849f4a4b46f66c6562bc46a
alessandroibes <alessandro.ibes@gmail.com>
2025-10-24 16:05:46 -0300

    feat: adding endpoint to save expenses

---
+++ src/FormacaoCSharp.CashFlow.Api/Controllers/ExpensesController.cs
@@ -0,0 +1,15 @@
+﻿using FormacaoCSharp.CashFlow.Communication.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FormacaoCSharp.CashFlow.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ExpensesController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Register([FromBody] RequestRegisterExpenseJson request)
+    {
+        return Created();
+    }
+}

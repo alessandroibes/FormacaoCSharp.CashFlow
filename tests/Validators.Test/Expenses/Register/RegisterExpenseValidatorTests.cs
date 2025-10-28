0917a476c8d686194c343e061d15291e393f2cd9
alessandroibes <alessandro.ibes@gmail.com>
2025-10-28 07:02:00 -0300

    tests: adding our first success test

---
+++ tests/Validators.Test/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -0,0 +1,28 @@
+﻿using FormacaoCSharp.CashFlow.Application.UseCases.Expenses.Register;
+using FormacaoCSharp.CashFlow.Communication.Requests;
+
+namespace Validators.Test.Expenses.Register;
+
+public class RegisterExpenseValidatorTests
+{
+    [Fact]
+    public void Success()
+    {
+        // Arrange
+        var validator = new RegisterExpenseValidator();
+        var request = new RequestRegisterExpenseJson
+        {
+            Amount = 100,
+            Date = DateTime.Now.AddDays(-1),
+            Description = "Description",
+            Title = "Apple",
+            PaymentType = FormacaoCSharp.CashFlow.Communication.Enums.PaymentType.CreditCard
+        };
+
+        // Act
+        var result = validator.Validate(request);
+
+        // Assert
+        Assert.True(result.IsValid);
+    }
+}

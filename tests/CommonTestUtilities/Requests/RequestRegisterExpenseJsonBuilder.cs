d935ea5f36f32ba63abea2ce1b8da626854c27ff
alessandroibes <alessandro.ibes@gmail.com>
2025-10-28 07:47:27 -0300

    tests: adding builder for request register expense with bogus

---
+++ tests/CommonTestUtilities/Requests/RequestRegisterExpenseJsonBuilder.cs
@@ -0,0 +1,18 @@
+﻿using Bogus;
+using FormacaoCSharp.CashFlow.Communication.Enums;
+using FormacaoCSharp.CashFlow.Communication.Requests;
+
+namespace CommonTestUtilities.Requests;
+
+public class RequestRegisterExpenseJsonBuilder
+{
+    public RequestRegisterExpenseJson Build()
+    {
+        return new Faker<RequestRegisterExpenseJson>()
+            .RuleFor(r => r.Title, faker => faker.Commerce.ProductName())
+            .RuleFor(r => r.Description, faker => faker.Commerce.ProductDescription())
+            .RuleFor(r => r.Date, faker => faker.Date.Past())
+            .RuleFor(r => r.PaymentType, faker => faker.PickRandom<PaymentType>())
+            .RuleFor(r => r.Amount, faker => faker.Random.Decimal(min: 1, max: 1000));
+    }
+}

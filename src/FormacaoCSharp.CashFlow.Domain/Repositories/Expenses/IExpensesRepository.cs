17c6b22cdb8dcc4b243fb99c1827fb622f838f77
alessandroibes <alessandro.ibes@gmail.com>
2025-10-30 14:00:09 -0300

    feat: adding interface for expense repository

---
+++ src/FormacaoCSharp.CashFlow.Domain/Repositories/Expenses/IExpensesRepository.cs
@@ -0,0 +1,8 @@
+﻿using FormacaoCSharp.CashFlow.Domain.Entities;
+
+namespace FormacaoCSharp.CashFlow.Domain.Repositories.Expenses;
+
+public interface IExpensesRepository
+{
+    void Add(Expense expense);
+}

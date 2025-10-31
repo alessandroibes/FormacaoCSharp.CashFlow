351761a0a30c5010fd7df2155b57b0052bc1d768
alessandroibes <alessandro.ibes@gmail.com>
2025-10-31 17:20:01 -0300

    feat: refactoring interfaces for repository spliting in context

---
+++ src/FormacaoCSharp.CashFlow.Domain/Repositories/Expenses/IExpensesReadOnlyRepository.cs
@@ -0,0 +1,9 @@
+﻿using FormacaoCSharp.CashFlow.Domain.Entities;
+
+namespace FormacaoCSharp.CashFlow.Domain.Repositories.Expenses;
+
+public interface IExpensesReadOnlyRepository
+{
+    Task<List<Expense>> GetAll();
+    Task<Expense?> GetById(long id);
+}

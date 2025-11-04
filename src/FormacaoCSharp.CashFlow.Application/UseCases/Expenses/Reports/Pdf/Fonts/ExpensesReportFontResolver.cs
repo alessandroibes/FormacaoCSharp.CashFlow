9ffb63aa6dc6539c79303f70eb9b6c5fde99ad0c
alessandroibes <alessandro.ibes@gmail.com>
2025-11-04 09:33:35 -0300

    feat: get bytes for fonts file

--- src/FormacaoCSharp.CashFlow.Application/UseCases/Expenses/Reports/Pdf/Fonts/ExpensesReportFontResolver.cs
+++ src/FormacaoCSharp.CashFlow.Application/UseCases/Expenses/Reports/Pdf/Fonts/ExpensesReportFontResolver.cs
@@ -1,4 +1,5 @@
 ﻿using PdfSharp.Fonts;
+using System.Reflection;
 
 namespace FormacaoCSharp.CashFlow.Application.UseCases.Expenses.Reports.Pdf.Fonts;
 
@@ -7,11 +8,28 @@ public class ExpensesReportFontResolver : IFontResolver
 {
     public byte[]? GetFont(string faceName)
     {
-        throw new NotImplementedException();
+        var stream = ReadFontFile(faceName);
+
+        stream ??= ReadFontFile(FontHelper.DEFAULT_FONT);
+
+        var length = (int)stream!.Length;
+
+        var data = new byte[length];
+
+        stream.Read(buffer: data, offset: 0, count: length);
+
+        return data;
     }
 
     public FontResolverInfo? ResolveTypeface(string familyName, bool bold, bool italic)
     {
         return new FontResolverInfo(familyName);
+    }
+
+    private Stream? ReadFontFile(string faceName)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+
+        return assembly.GetManifestResourceStream($"FormacaoCSharp.CashFlow.Application.UseCases.Expenses.Reports.Pdf.Fonts.{faceName}.ttf");
     }
 }

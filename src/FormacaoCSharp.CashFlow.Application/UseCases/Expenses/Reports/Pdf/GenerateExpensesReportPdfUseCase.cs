77691baf4654f2309a8934a605f6c373a5c24c99
alessandroibes <alessandro.ibes@gmail.com>
2025-11-04 08:23:54 -0300

    feat: starting font resolver

--- src/FormacaoCSharp.CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
+++ src/FormacaoCSharp.CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
@@ -1,4 +1,6 @@
-﻿using FormacaoCSharp.CashFlow.Domain.Repositories.Expenses;
+﻿using FormacaoCSharp.CashFlow.Application.UseCases.Expenses.Reports.Pdf.Fonts;
+using FormacaoCSharp.CashFlow.Domain.Repositories.Expenses;
+using PdfSharp.Fonts;
 
 namespace FormacaoCSharp.CashFlow.Application.UseCases.Expenses.Reports.Pdf;
 
@@ -10,6 +12,8 @@ public class GenerateExpensesReportPdfUseCase : IGenerateExpensesReportPdfUseCas
     public GenerateExpensesReportPdfUseCase(IExpensesReadOnlyRepository repository)
     {
         _repository = repository;
+
+        GlobalFontSettings.FontResolver = new ExpensesReportFontResolver();
     }
 
     public async Task<byte[]> Execute(DateOnly month)

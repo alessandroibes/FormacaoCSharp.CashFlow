db89d9e9216fb401a28f9190fa24d4e4ae0a3741
alessandroibes <alessandro.ibes@gmail.com>
2025-11-05 13:04:19 -0300

    feat: starting flow to register users

---
+++ src/FormacaoCSharp.CashFlow.Application/UseCases/Users/Register/IRegisterUserUseCase.cs
@@ -0,0 +1,9 @@
+﻿using FormacaoCSharp.CashFlow.Communication.Requests;
+using FormacaoCSharp.CashFlow.Communication.Responses;
+
+namespace FormacaoCSharp.CashFlow.Application.UseCases.Users.Register;
+
+public interface IRegisterUserUseCase
+{
+    Task<ResponseRegisteredUserJson> Execute(RequestRegisterUserJson request);
+}

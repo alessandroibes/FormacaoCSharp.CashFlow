98680c86002eaf433d649276cdc3cb047e1b8be9
alessandroibes <alessandro.ibes@gmail.com>
2025-11-06 11:00:29 -0300

    feat: conclunding token test

--- src/FormacaoCSharp.CashFlow.Infrastructure/DependencyInjecyionExtension.cs
+++ src/FormacaoCSharp.CashFlow.Infrastructure/DependencyInjecyionExtension.cs
@@ -2,8 +2,10 @@
 using FormacaoCSharp.CashFlow.Domain.Repositories.Expenses;
 using FormacaoCSharp.CashFlow.Domain.Repositories.User;
 using FormacaoCSharp.CashFlow.Domain.Security.Cryptography;
+using FormacaoCSharp.CashFlow.Domain.Security.Tokens;
 using FormacaoCSharp.CashFlow.Infrastructure.DataAccess;
 using FormacaoCSharp.CashFlow.Infrastructure.DataAccess.Repositories;
+using FormacaoCSharp.CashFlow.Infrastructure.Security.Tokens;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -15,9 +17,18 @@ public static class DependencyInjecyionExtension
     public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
         AddDbContext(services, configuration);
+        AddToken(services, configuration);
         AddRepositories(services);
 
         services.AddScoped<IPasswordEncripter, Security.Cryptography.BCrypt>();
+    }
+
+    private static void AddToken(IServiceCollection services, IConfiguration configuration)
+    {
+        var expirationTimeMinutes = configuration.GetValue<uint>("Settings:Jwt:ExpiresMinutes");
+        var signingKey = configuration.GetValue<string>("Settings:Jwt:SigningKey");
+
+        services.AddScoped<IAccessTokenGenerator>(config => new JwtTokenGenerator(expirationTimeMinutes, signingKey!));
     }
 
     private static void AddRepositories(IServiceCollection services)

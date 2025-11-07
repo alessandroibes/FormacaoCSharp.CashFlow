2ea89c37cdc160c51614257298be744bbbaffe75
alessandroibes <alessandro.ibes@gmail.com>
2025-11-07 15:56:48 -0300

    test: finishing validator tests for register user

--- tests/Validators.Test/Users/PasswordValidatorTest.cs
+++ tests/Validators.Test/Users/PasswordValidatorTest.cs
@@ -1,5 +1,34 @@
-﻿namespace Validators.Test.Users;
+﻿using FluentAssertions;
+using FluentValidation;
+using FormacaoCSharp.CashFlow.Application.UseCases.Users;
+using FormacaoCSharp.CashFlow.Communication.Requests;
+
+namespace Validators.Test.Users;
 
 public class PasswordValidatorTest
 {
+    [Theory]
+    [InlineData("")]
+    [InlineData("      ")]
+    [InlineData(null)]
+    [InlineData("a")]
+    [InlineData("aa")]
+    [InlineData("aaa")]
+    [InlineData("aaaa")]
+    [InlineData("aaaaa")]
+    [InlineData("aaaaaa")]
+    [InlineData("aaaaaaa")]
+    [InlineData("aaaaaaaa")]
+    [InlineData("AAAAAAAA")]
+    [InlineData("Aaaaaaaa")]
+    [InlineData("Aaaaaaa1")]
+    public void Error_Password_Invalid(string password)
+    {
+        var validator = new PasswordValidator<RequestRegisterUserJson>();
+
+        var result = validator
+            .IsValid(new ValidationContext<RequestRegisterUserJson>(new RequestRegisterUserJson()), password);
+
+        result.Should().BeFalse();
+    }
 }

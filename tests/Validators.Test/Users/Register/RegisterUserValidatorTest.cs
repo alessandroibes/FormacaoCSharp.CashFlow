785dc25707e8067d13fabd3c475b44cd12164659
alessandroibes <alessandro.ibes@gmail.com>
2025-11-07 15:37:38 -0300

    test: starting test to user validator

---
+++ tests/Validators.Test/Users/Register/RegisterUserValidatorTest.cs
@@ -0,0 +1,57 @@
+﻿using CommonTestUtilities.Requests;
+using FluentAssertions;
+using FormacaoCSharp.CashFlow.Application.UseCases.Users.Register;
+using FormacaoCSharp.CashFlow.Exception;
+
+namespace Validators.Test.Users.Register;
+
+public class RegisterUserValidatorTest
+{
+    [Fact]
+    public void Success()
+    {
+        // Arrange
+        var validator = new RegisterUserValidator();
+        var request = RequestRegisterUserJsonBuilder.Build();
+
+        // Act
+        var result = validator.Validate(request);
+
+        // Assert
+        result.IsValid.Should().BeTrue();
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("      ")]
+    [InlineData(null)]
+    public void Error_Name_Empty(string name)
+    {
+        var validator = new RegisterUserValidator();
+        var request = RequestRegisterUserJsonBuilder.Build();
+        request.Name = name;
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle()
+            .And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.NAME_EMPTY));
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("      ")]
+    [InlineData(null)]
+    public void Error_Email_Empty(string email)
+    {
+        var validator = new RegisterUserValidator();
+        var request = RequestRegisterUserJsonBuilder.Build();
+        request.Email = email;
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle()
+            .And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.EMAIL_EMPTY));
+    }
+}
